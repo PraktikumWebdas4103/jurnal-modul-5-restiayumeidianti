4697450bec3d2010afa6f476a51d3236d84ac491
restiayumeidianti <43517877+restiayumeidianti@users.noreply.github.com>
2018-10-02 16:56:15 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,29 @@
 <?php
+<form action="" method="POST">
+	<center>
+		<table>
+		<tr>
+		<td> Inputkan Kata </td>
+		<td> : </td>
+		<td><input type="text" name="kata"></td>
+		<td><input type="submit" name="submit" value="simpan">
+	</tr>
 
+</center>
+</table>
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+	$kata = $_POST['kata'];
+	$jmlh = str_word_count($kata);
+
+	if (str_word_count($_POST['kata']) < 5) {
+		echo "Komentar harus lebih dari 5 kata";
+	}
+	else{
+		$kata = $_POST['kata'];
+		echo "Jumlah kata : $jmlh.</br>";
+	}
+}
 ?>
